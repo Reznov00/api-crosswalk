1ca6c3b29c1948568527f49c07c03418ac1e9e29
stack-usman <113902192+stack-usman@users.noreply.github.com>
2024-07-05 16:29:14 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,46 +7,47 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get("/", () => {
-  console.log("SERVER LIVE");
+app.get("/", (req, res) => {
+  res.send("SERVER LIVE");
 });
 
 // Routes
 app.get("/institutions", async (req, res) => {
-  // Return a list of institutions
-  const response = await axios.get(
-    `${process.env.PYTHON_API_URL}/institutions`
-  );
-  console.log(response);
-  if (response.status === 200) {
-    return res.status(200).json(response.data);
+  try {
+    const response = await axios.get(
+      `${process.env.PYTHON_API_URL}/institutions`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch institutions" });
   }
-  return res.status(500).json({ error: "Failed to fetch institutions" });
 });
 
 app.post("/courses", async (req, res) => {
   const { institutionIds } = req.body;
   let query = "";
   institutionIds.forEach((ins) => (query += "institution_id=" + ins + "&"));
-  const response = await axios.get(
-    `${process.env.PYTHON_API_URL}/courses?${query}`
-  );
-  if (response.status === 200) {
-    return res.status(200).json(response.data);
+  try {
+    const response = await axios.get(
+      `${process.env.PYTHON_API_URL}/courses?${query}`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ error: "Failed to fetch courses" });
   }
-  return res.status(400).json({ error: "Failed to fetch courses" });
 });
 
 app.post("/search", async (req, res) => {
   const { institution_id, course_code } = req.body;
-  const response = await axios.post(`${process.env.PYTHON_API_URL}/search`, {
-    institution_id,
-    course_code,
-  });
-  if (response.status === 200) {
-    return res.status(200).json(response.data);
+  try {
+    const response = await axios.post(
+      `${process.env.PYTHON_API_URL}/search`,
+      { institution_id, course_code }
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ error: "Failed to search courses" });
   }
-  return res.status(400).json({ error: "Failed to search courses" });
 });
 
 // Start the server
